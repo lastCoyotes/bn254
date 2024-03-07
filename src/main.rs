d6226ff08a2b46e774b47e4fc6a1c830371d4d4b
Coyotes <lastcoyotes@gmail.com>
2024-03-07 02:47:02 -0600

    added mul function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![feature(bigint_helper_methods)]
 use std::convert::TryInto;
 
 pub struct UInt256 {
@@ -36,7 +37,71 @@ impl UInt256 {
         }
         
     }
+    
+    pub fn mul_step(z: u64, x: u64, y: u64, carry: u64) -> (u64, u64) {
+        let mut hi: u64;
+        let mut lo: u64;
+        let mut carry_flag: bool;
 
+        (lo, hi) = x.widening_mul(y);
+        (lo, carry_flag) = lo.overflowing_add(carry);
+        (hi, _) = hi.overflowing_add(carry_flag as u64);
+        (lo, carry_flag) = lo.overflowing_add(z);
+        (hi, _) = hi.overflowing_add(carry_flag as u64);
+
+        (hi, lo)
+    }
+
+    pub fn mul_hop(z: u64, x: u64, y: u64) -> (u64, u64) {
+        let mut hi: u64;
+        let mut lo: u64;
+        let carry: bool;
+
+        (lo, hi) = x.widening_mul(y);
+        (lo, carry) = lo.overflowing_add(z);
+        (hi, _) = hi.carrying_add(0, carry);
+
+        (hi, lo)
+    }
+
+    pub fn mul(&self, other: &Self) -> [u64; 8] {
+        let x = &self.limbs;
+        let y = other.limbs;
+        let mut res = [0u64; 8];
+        let res1: u64;
+        let mut res2: u64;
+        let mut res3: u64;
+        let mut res4: u64;
+        let res5: u64;
+        let mut carry: u64;
+        let carry4: u64;
+        let carry5: u64;
+        let carry6: u64;
+        
+        (res[0], carry) = x[0].widening_mul(y[0]);
+        (carry, res1) = Self::mul_hop(carry, x[1], y[0]);
+        (carry, res2) = Self::mul_hop(carry, x[2], y[0]);
+        (carry4, res3) = Self::mul_hop(carry, x[3], y[0]);
+        
+        (carry, res[1]) = Self::mul_hop(res1, x[0], y[1]);
+        (carry, res2) = Self::mul_step(res2, x[1], y[1], carry);
+        (carry, res3) = Self::mul_step(res3, x[2], y[1], carry);
+        (carry5, res4) = Self::mul_step(carry4, x[3], y[1], carry);
+        
+        (carry, res[2]) = Self::mul_hop(res2, x[0], y[2]);
+        (carry, res3) = Self::mul_step(res3, x[1], y[2], carry);
+        (carry, res4) = Self::mul_step(res4, x[2], y[2], carry);
+        (carry6, res5) = Self::mul_step(carry5, x[3], y[2], carry);
+        
+        (carry, res[3]) = Self::mul_hop(res3, x[0], y[3]);
+        (carry, res[4]) = Self::mul_step(res4, x[1], y[3], carry);
+        (carry, res[5]) = Self::mul_step(res5, x[2], y[3], carry);
+        (res[7], res[6]) = Self::mul_step(carry6, x[3], y[3], carry);
+        
+        res
+
+    }
+    
 
     /*
     // increment UInt256 by one--handles overflow
@@ -51,33 +116,33 @@ impl UInt256 {
     */
 }
 
-imple AddAssign for UInt256 {
-    fn add_assign(&mut self, other: Self) {
-        
-    }
-}
+// TODO: explore implementing add assign for UInt256 later
 
 fn main() {
-    let mut _test = UInt256 {
+    let _test = UInt256 {
         limbs: [u64::MAX, u64::MAX, u64::MAX, 0]
     };
 
-    let mut _test2 = UInt256 {
-        limbs: [0, u64::MAX, 0, 0]
+    let test2 = UInt256 {
+        limbs: [u64::MAX, 0, 0, 0]
     };
 
-    let mut _test3 = UInt256 {
-        limbs: [0, 0, 0, 0]
+    let test3 = UInt256 {
+        limbs: [u64::MAX, 1, 0, 0]
     };
 
-    //println!("u64 MAX: {}", u64::MAX);
+    //println!("u64 MAX: {:#066b}", u64::MAX);
+    //
+    //println!("TEST: {:?}", test.limbs);
+    println!("TEST2: {:?}", test2.limbs);
+    /*
+    println!("SUM: {:?}", test.add(&test2).limbs);
 
-    println!("_TEST: {:?}", _test.limbs);
-    println!("TEST2: {:?}", _test2.limbs);
-    
-    println!("SUM: {:?}", _test.add(&_test2).limbs);
+    println!("TEST3: {:?}", test3.limbs);
+    test3.add_u64(u64::MAX);
+    println!("TEST3 + u64 MAX: {:?}", test3.limbs);
+    */
 
-    println!("TEST3: {:?}", _test3.limbs);
-    _test3.add_u64(u64::MAX);
-    println!("TEST3 + u64 MAX: {:?}", _test3.limbs);
+    //test2.mul(test3);
+    println!("PRODUCT: {:?}", test2.mul(&test3));
 }
